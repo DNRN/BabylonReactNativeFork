e9e01f1f765d1f4c391bae871f1cccc1e5e12dab
Dennis Christensen <dennis@entropyfox.com>
2024-05-21 22:14:44 +0200

    Anchor crash

--- Apps/Playground/0.71/index.js
+++ Apps/Playground/0.71/index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from '../playground-shared/App';
+import App from './App';
 import {name as appName} from '../playground-shared/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
